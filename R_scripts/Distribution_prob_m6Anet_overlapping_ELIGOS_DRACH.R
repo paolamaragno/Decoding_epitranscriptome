5cb9c95859281dbc72e8f04a697845c9c0f99670
paolamaragno <158480032+paolamaragno@users.noreply.github.com>
2024-02-13 19:00:18 +0100

    Update Distribution_prob_m6Anet_overlapping_ELIGOS_DRACH.R

--- R_scripts/Distribution_prob_m6Anet_overlapping_ELIGOS_DRACH.R
+++ R_scripts/Distribution_prob_m6Anet_overlapping_ELIGOS_DRACH.R
@@ -148,6 +148,35 @@ ELIGOS_results_min05_mag05_min1 <- function(path_directory, p, ap, OR) {
   return(gr)
 }
 
+# function to remove ELIGOS hits overlapping with the coordinates of the SNPs of SUM159 and k562 cell lines
+rm_SNPs <- function(path_SNPs_SUM, path_SNPs_k562, hits) {
+    
+    # SNPs SUM159 on hg38
+    vcf_SUM <-read.table(path_SNPs_SUM)
+    
+    grange_vcf_SUM <- GRanges(seqnames = vcf_SUM$V1,
+                              ranges = IRanges(start = vcf_SUM$V2, end=vcf_SUM$V2))
+    grange_vcf_SUM <- resize(grange_vcf_SUM, 5, 'center')
+    
+    length(hits)
+    
+    over_hits_eligos_vcf_SUM <- suppressWarnings(findOverlaps(hits,grange_vcf_SUM, type = 'any', ignore.strand=TRUE))
+    hits_without_SNPs_SUM <- hits[-unique(queryHits(over_hits_eligos_vcf_SUM))]
+    
+    # SNPs k562 on hg38
+    vcf_k562 <-read.table(path_SNPs_k562)
+    
+    grange_vcf_k562 <- GRanges(seqnames = gsub('chr', '', vcf_k562$V1),
+                               ranges = IRanges(start = vcf_k562$V2, end=vcf_k562$V2))
+    grange_vcf_k562 <- resize(grange_vcf_k562, 5, 'center')
+    
+    over_hits_eligos_vcf_k562 <- suppressWarnings(findOverlaps(hits_without_SNPs_SUM,grange_vcf_k562, type = 'any', ignore.strand=TRUE))
+    length(unique(queryHits(over_hits_eligos_vcf_k562)))
+    hits_without_SNPs_SUM_k562 <- hits_without_SNPs_SUM[-unique(queryHits(over_hits_eligos_vcf_k562))]
+    
+    return(hits_without_SNPs_SUM_k562)
+}
+
 # create a folder /hits_ELIGOS/ inside path_directory in which you will save the hits of each fraction 
 # confirmed by at least 5 samplings
 ELIGOS_results <- function(path_directory, gr) {
@@ -187,23 +216,23 @@ ELIGOS_results <- function(path_directory, gr) {
   gr_eligos_nucleo <- unlist(gr_eligos_nucleo)
   gr_eligos_cyto <- unlist(gr_eligos_cyto)
   
-  eligos_chr_ass_all_replicate_10nt <- c(gr_eligos_chr_ass[[1]],gr_eligos_chr_ass[[2]],gr_eligos_chr_ass[[3]],gr_eligos_chr_ass[[4]],gr_eligos_chr_ass[[5]],
-                                         gr_eligos_chr_ass[[6]],gr_eligos_chr_ass[[7]],gr_eligos_chr_ass[[8]],gr_eligos_chr_ass[[9]],gr_eligos_chr_ass[[10]])
+  eligos_chr_ass_all_samplings <- c(gr_eligos_chr_ass[[1]],gr_eligos_chr_ass[[2]],gr_eligos_chr_ass[[3]],gr_eligos_chr_ass[[4]],gr_eligos_chr_ass[[5]])
+  eligos_chr_ass_all_samplings_without_SNPs <- rm_SNPs("/Users/paolamarango/Documents/IVT_SNPs/SUM159/SNPs_SUM_hg38.bed","/Users/paolamarango/Documents/IVT_SNPs/k562/IVT_k562_hg38.vcf",eligos_chr_ass_all_samplings)
   
-  confirmed_by_chr <- data.frame(num_samplings=seq(1,10), num_hits=rep(0,10))
-  confirmed_by_at_least_5_chr <- c()
+  confirmed_by_chr <- data.frame(num_samplings=seq(1,5), num_hits=rep(0,5))
+  confirmed_by_5_chr <- c()
   
-  # identify the hits present in at least 5 samplings (any type of overlap)
-  for (r in 1:length(eligos_chr_ass_all_replicate_10nt)) {
-    gr_r_rep <- queryHits(findOverlaps(eligos_chr_ass_all_replicate_10nt, eligos_chr_ass_all_replicate_10nt[r], type='any')) 
-    confirmed_by_chr[length(unique(eligos_chr_ass_all_replicate_10nt[gr_r_rep]$rep)),2] <- confirmed_by_chr[length(unique(eligos_chr_ass_all_replicate_10nt[gr_r_rep]$rep)),2] +1
-    if (length(unique(eligos_chr_ass_all_replicate_10nt[gr_r_rep]$rep)) >= 5) {
-      confirmed_by_at_least_5_chr <- c(confirmed_by_at_least_5_chr, r)
+  # identify the hits present in 5 samplings (any type of overlap)
+  for (r in 1:length(eligos_chr_ass_all_samplings_without_SNPs)) {
+    gr_r_rep <- queryHits(findOverlaps(eligos_chr_ass_all_samplings_without_SNPs, eligos_chr_ass_all_samplings_without_SNPs[r], type='any')) 
+    confirmed_by_chr[length(unique(eligos_chr_ass_all_samplings_without_SNPs[gr_r_rep]$rep)),2] <- confirmed_by_chr[length(unique(eligos_chr_ass_all_samplings_without_SNPs[gr_r_rep]$rep)),2] +1
+    if (length(unique(eligos_chr_ass_all_samplings_without_SNPs[gr_r_rep]$rep)) == 5) {
+      confirmed_by_5_chr <- c(confirmed_by_5_chr, r)
     }
   }
   
-  # the hits confirmed in at least 5 samplings are resized to the original coordinates returned by ELIGOS 
-  hits_eligos_chr_ass_confirmed_5 <- resize(eligos_chr_ass_all_replicate_10nt[confirmed_by_at_least_5_chr], 2, fix = 'center')
+  # the hits confirmed in 5 samplings are resized to the original coordinates returned by ELIGOS 
+  hits_eligos_chr_ass_confirmed_5 <- resize(eligos_chr_ass_all_samplings_without_SNPs[confirmed_by_5_chr], 2, fix = 'center')
   hits_eligos_chr_ass_confirmed_5 <- reduce(hits_eligos_chr_ass_confirmed_5, ignore.strand = FALSE)
   print(table(width(hits_eligos_chr_ass_confirmed_5)))
   # resize the merged hits to have a width at least of 10 nucleotides
@@ -222,21 +251,21 @@ ELIGOS_results <- function(path_directory, gr) {
   }
   mcols(hits_eligos_chr_ass_confirmed_5) <- cbind(mcols(hits_eligos_chr_ass_confirmed_5), gene_id = gene_ids)
   
-  eligos_nucleo_all_replicate_10nt <- c(gr_eligos_nucleo[[1]],gr_eligos_nucleo[[2]],gr_eligos_nucleo[[3]],gr_eligos_nucleo[[4]],gr_eligos_nucleo[[5]],
-                                        gr_eligos_nucleo[[6]],gr_eligos_nucleo[[7]],gr_eligos_nucleo[[8]],gr_eligos_nucleo[[9]],gr_eligos_nucleo[[10]])
+  eligos_nucleo_all_samplings <- c(gr_eligos_nucleo[[1]],gr_eligos_nucleo[[2]],gr_eligos_nucleo[[3]],gr_eligos_nucleo[[4]],gr_eligos_nucleo[[5]])
+  eligos_nucleo_all_samplings_without_SNPs <- rm_SNPs("/Users/paolamarango/Documents/IVT_SNPs/SUM159/SNPs_SUM_hg38.bed","/Users/paolamarango/Documents/IVT_SNPs/k562/IVT_k562_hg38.vcf",eligos_nucleo_all_samplings)
   
-  confirmed_by_nucleo <- data.frame(num_samplings=seq(1,10), num_hits=rep(0,10))
-  confirmed_by_at_least_5_nucleo <- c()
+  confirmed_by_nucleo <- data.frame(num_samplings=seq(1,5), num_hits=rep(0,5))
+  confirmed_by_5_nucleo <- c()
   
-  for (r in 1:length(eligos_nucleo_all_replicate_10nt)) {
-    gr_r_rep <- queryHits(findOverlaps(eligos_nucleo_all_replicate_10nt, eligos_nucleo_all_replicate_10nt[r], type='any')) 
-    confirmed_by_nucleo[length(unique(eligos_nucleo_all_replicate_10nt[gr_r_rep]$rep)),2] <- confirmed_by_nucleo[length(unique(eligos_nucleo_all_replicate_10nt[gr_r_rep]$rep)),2] +1
-    if (length(unique(eligos_nucleo_all_replicate_10nt[gr_r_rep]$rep)) >= 5) {
-      confirmed_by_at_least_5_nucleo <- c(confirmed_by_at_least_5_nucleo, r)
+  for (r in 1:length(eligos_nucleo_all_samplings_without_SNPs)) {
+    gr_r_rep <- queryHits(findOverlaps(eligos_nucleo_all_samplings_without_SNPs, eligos_nucleo_all_samplings_without_SNPs[r], type='any')) 
+    confirmed_by_nucleo[length(unique(eligos_nucleo_all_samplings_without_SNPs[gr_r_rep]$rep)),2] <- confirmed_by_nucleo[length(unique(eligos_nucleo_all_samplings_without_SNPs[gr_r_rep]$rep)),2] +1
+    if (length(unique(eligos_nucleo_all_samplings_without_SNPs[gr_r_rep]$rep)) == 5) {
+      confirmed_by_5_nucleo <- c(confirmed_by_5_nucleo, r)
     }
   }
   
-  hits_eligos_nucleo_confirmed_5 <- resize(eligos_nucleo_all_replicate_10nt[confirmed_by_at_least_5_nucleo], 2, fix = 'center')
+  hits_eligos_nucleo_confirmed_5 <- resize(eligos_nucleo_all_samplings_without_SNPs[confirmed_by_5_nucleo], 2, fix = 'center')
   hits_eligos_nucleo_confirmed_5 <- reduce(hits_eligos_nucleo_confirmed_5, ignore.strand = FALSE)
   print(table(width(hits_eligos_nucleo_confirmed_5)))
   for (i in 1:length(hits_eligos_nucleo_confirmed_5)) {
@@ -253,21 +282,21 @@ ELIGOS_results <- function(path_directory, gr) {
   }
   mcols(hits_eligos_nucleo_confirmed_5) <- cbind(mcols(hits_eligos_nucleo_confirmed_5), gene_id = gene_ids)
   
-  eligos_cyto_all_replicate_10nt <- c(gr_eligos_cyto[[1]],gr_eligos_cyto[[2]],gr_eligos_cyto[[3]],gr_eligos_cyto[[4]],gr_eligos_cyto[[5]],
-                                      gr_eligos_cyto[[6]],gr_eligos_cyto[[7]],gr_eligos_cyto[[8]],gr_eligos_cyto[[9]],gr_eligos_cyto[[10]])
+  eligos_cyto_all_samplings <- c(gr_eligos_cyto[[1]],gr_eligos_cyto[[2]],gr_eligos_cyto[[3]],gr_eligos_cyto[[4]],gr_eligos_cyto[[5]])
+  eligos_cyto_all_samplings_without_SNPs <- rm_SNPs("/Users/paolamarango/Documents/IVT_SNPs/SUM159/SNPs_SUM_hg38.bed","/Users/paolamarango/Documents/IVT_SNPs/k562/IVT_k562_hg38.vcf",eligos_cyto_all_samplings)
   
-  confirmed_by_cyto <- data.frame(num_samplings=seq(1,10), num_hits=rep(0,10))
-  confirmed_by_at_least_5_cyto <- c()
+  confirmed_by_cyto <- data.frame(num_samplings=seq(1,5), num_hits=rep(0,5))
+  confirmed_by_5_cyto <- c()
   
-  for (r in 1:length(eligos_cyto_all_replicate_10nt)) {
-    gr_r_rep <- queryHits(findOverlaps(eligos_cyto_all_replicate_10nt, eligos_cyto_all_replicate_10nt[r], type='any')) 
-    confirmed_by_cyto[length(unique(eligos_cyto_all_replicate_10nt[gr_r_rep]$rep)),2] <- confirmed_by_cyto[length(unique(eligos_cyto_all_replicate_10nt[gr_r_rep]$rep)),2] +1
-    if (length(unique(eligos_cyto_all_replicate_10nt[gr_r_rep]$rep)) >= 5) {
-      confirmed_by_at_least_5_cyto <- c(confirmed_by_at_least_5_cyto, r)
+  for (r in 1:length(eligos_cyto_all_samplings_without_SNPs)) {
+    gr_r_rep <- queryHits(findOverlaps(eligos_cyto_all_samplings_without_SNPs, eligos_cyto_all_samplings_without_SNPs[r], type='any')) 
+    confirmed_by_cyto[length(unique(eligos_cyto_all_samplings_without_SNPs[gr_r_rep]$rep)),2] <- confirmed_by_cyto[length(unique(eligos_cyto_all_samplings_without_SNPs[gr_r_rep]$rep)),2] +1
+    if (length(unique(eligos_cyto_all_samplings_without_SNPs[gr_r_rep]$rep)) == 5) {
+      confirmed_by_5_cyto <- c(confirmed_by_5_cyto, r)
     }
   }
   
-  hits_eligos_cyto_confirmed_5 <- resize(eligos_cyto_all_replicate_10nt[confirmed_by_at_least_5_cyto], 2, fix = 'center')
+  hits_eligos_cyto_confirmed_5 <- resize(eligos_cyto_all_samplings_without_SNPs[confirmed_by_5_cyto], 2, fix = 'center')
   hits_eligos_cyto_confirmed_5 <- reduce(hits_eligos_cyto_confirmed_5, ignore.strand = FALSE)
   print(table(width(hits_eligos_cyto_confirmed_5)))
   for (i in 1:length(hits_eligos_cyto_confirmed_5)) {
